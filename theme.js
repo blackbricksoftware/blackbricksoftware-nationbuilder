cdb5d6fd7f89f528978c0cfb26b2382c84f4bab7
David Hayes <david@blackbricksoftware.com>
2014-11-17 18:20:31 -0600

    style updates

---
+++ theme.js
@@ -0,0 +1,18 @@
+/*jQuery(function($){
+  //~ $('#page-features').flexslider({
+	//~ start: function(slider) {
+		//~ console.log('asdf');
+	//~ },
+//~ });
+  
+  //~ console.log());
+  //~ console.log($('#page-features').data('flexslider'));
+	
+	if ($('#page-features').length>0) {
+		$('#middle').css({
+			'margin-top': $('#page-features').outerHeight()+'px',
+		});
+	}
+	
+});
+*/
